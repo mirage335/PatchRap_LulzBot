5010c8615b66032aee015d74fce83b7af9b53e51
mirage335 <spamfreemirage335@gmail.com>
2020-08-30 17:47:46 -0400

    Draft.

---
+++ _pr-stepper-wtb/_pr-stepper-wtb.cmd
@@ -0,0 +1,44 @@
+# Pin name action command file
+
+# Start of element RED
+ChangePinName(RED, 1, 1)
+ChangePinName(RED, 4, 4)
+ChangePinName(RED, 2, 2)
+ChangePinName(RED, 3, 3)
+
+# Start of element BLACK
+ChangePinName(BLACK, 1, 1)
+ChangePinName(BLACK, 4, 4)
+ChangePinName(BLACK, 2, 2)
+ChangePinName(BLACK, 3, 3)
+
+# Start of element GREEN
+ChangePinName(GREEN, 1, 1)
+ChangePinName(GREEN, 4, 4)
+ChangePinName(GREEN, 2, 2)
+ChangePinName(GREEN, 3, 3)
+
+# Start of element BLUE
+ChangePinName(BLUE, 1, 1)
+ChangePinName(BLUE, 4, 4)
+ChangePinName(BLUE, 2, 2)
+ChangePinName(BLUE, 3, 3)
+
+# Start of element R1
+ChangePinName(R1, 1, 1)
+ChangePinName(R1, 2, 2)
+
+# Start of element STEP
+ChangePinName(STEP, 13, 13)
+ChangePinName(STEP, 12, 12)
+ChangePinName(STEP, 11, 11)
+ChangePinName(STEP, 10, 10)
+ChangePinName(STEP, 9, 9)
+ChangePinName(STEP, 8, 8)
+ChangePinName(STEP, 7, 7)
+ChangePinName(STEP, 6, 6)
+ChangePinName(STEP, 5, 5)
+ChangePinName(STEP, 4, 4)
+ChangePinName(STEP, 3, 3)
+ChangePinName(STEP, 2, 2)
+ChangePinName(STEP, 1, 1)
